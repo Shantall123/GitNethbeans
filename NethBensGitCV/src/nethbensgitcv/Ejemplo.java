c029d9b7c91ff21aaf2a53efbf6aaf7b0c645dd0
Shantall <shantaguirre0601@gmail.com>
2026-05-08 09:43:18 -0500

    Crea clase de ejemplo

---
+++ NethBensGitCV/src/nethbensgitcv/Ejemplo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nethbensgitcv;
+
+/**
+ *
+ * @author Shanthallleon
+ */
+public class Ejemplo {
+    
+    //hola mundo
+    
+}
